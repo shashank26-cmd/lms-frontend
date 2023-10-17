e83474ed29c4b54a974674cb5813e1e7026b8541
shashank26-cmd <shashankbud80@gmail.com>
2023-10-17 07:39:01 +0530

    Login page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Routes,Route } from 'react-router-dom'
 import AboutUs from './Pages/AboutUs'
 import Homepage  from './Pages/HomePage'
 import Notfound from './Pages/Notfound'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login';
 function App() {
  
   return (
@@ -15,6 +17,9 @@ function App() {
 <Route path="/" element={<Homepage />}></Route>
 <Route path='/about' element={<AboutUs />}></Route>
 <Route path='*'  element={<Notfound />}></Route>
+<Route path='/signup' element={<Signup />}></Route>
+<Route path='/login' element={<Login />}></Route>
+
 
   </Routes>
   </>
